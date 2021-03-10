4e2d7f3636b29958d312f9bbc29da5a9e9c5b5d0
Victor Colin Amador <victor.y.asi@gmail.com>
2021-03-09 22:28:31 -0600

    Updated MainActivity to use convenience layer APIs.

--- samples/sample-chat-app/src/main/java/com/azure/android/communication/chat/sampleapp/MainActivity.java
+++ samples/sample-chat-app/src/main/java/com/azure/android/communication/chat/sampleapp/MainActivity.java
@@ -3,26 +3,11 @@
 
 package com.azure.android.communication.chat.sampleapp;
 
-import com.azure.android.communication.chat.*;
-import com.azure.android.communication.chat.models.AddChatParticipantsRequest;
-import com.azure.android.communication.chat.models.AddChatParticipantsResult;
-import com.azure.android.communication.chat.models.ChatMessageType;
-import com.azure.android.communication.chat.models.ChatParticipant;
-import com.azure.android.communication.chat.models.ChatThread;
-import com.azure.android.communication.chat.models.CommunicationIdentifierModel;
-import com.azure.android.communication.chat.models.CommunicationUserIdentifierModel;
-import com.azure.android.communication.chat.models.CreateChatThreadRequest;
-import com.azure.android.communication.chat.models.CreateChatThreadResult;
-import com.azure.android.communication.chat.models.SendChatMessageRequest;
-import com.azure.android.core.http.Callback;
-import com.azure.android.core.http.HttpHeader;
-import com.azure.android.core.http.ServiceClient;
-import com.azure.android.core.http.interceptor.UserAgentInterceptor;
-import com.azure.android.core.http.responsepaging.AsyncPagedDataCollection;
-import com.azure.android.core.util.paging.Page;
+import com.azure.android.communication.chat.handwritten.ChatClient;
+import com.azure.android.communication.chat.handwritten.ChatClientBuilder;
+import com.azure.android.core.http.policy.UserAgentPolicy;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import android.os.Bundle;
 import android.util.Log;
@@ -31,13 +16,7 @@ import android.widget.Toast;
 
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.Response;
-
 public class MainActivity extends AppCompatActivity {
-
     private ChatClient chatClient;
     private int eventHandlerCalled;
     private JSONObject eventPayload;
@@ -50,11 +29,11 @@ public class MainActivity extends AppCompatActivity {
     private final String endpoint = "https://<your_acs_instance>.communication.azure.net";
     private final String listenerId = "testListener";
     private final String sdkVersion = "1.0.0-beta.6";
+    private static final String APPLICATION_ID = "acs-sample-app";
     private static final String SDK_NAME = "azure-communication-com.azure.android.communication.chat";
     private static final String TAG = "[Chat Test App]";
 
-
-    private void log(String msg) {
+    private void logAndToast(String msg) {
         Log.i(TAG, msg);
         Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
     }
@@ -68,25 +47,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startChatClient() {
-        log("Creating chat client");
+        logAndToast("Creating chat client");
 
-        UserAgentInterceptor userAgentInterceptor = new UserAgentInterceptor(
-            this.getApplicationContext(),
+        UserAgentPolicy userAgentPolicy = new UserAgentPolicy(
+            APPLICATION_ID,
             SDK_NAME,
             sdkVersion
         );
 
         // Initialize the chat client
-        chatClient = new ChatClient.Builder()
-            .serviceClientBuilder(new ServiceClient.Builder())
+        chatClient = new ChatClientBuilder()
             .endpoint(endpoint)
-            .userAgentInterceptor(userAgentInterceptor)
+            .addPolicy(userAgentPolicy)
             .realtimeNotificationParams(this.getApplicationContext(), userAccessToken)
             .build();
     }
 
     public void actionStartRealtimeNotification(View view) {
         Log.i(TAG, "Starting real time notification");
+
         try {
             chatClient.startRealtimeNotifications();
         } catch (Exception e) {
@@ -96,20 +75,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void actionRegisterATestListener(View view) {
         // Act - subscribe
-        log("Register a test listener");
-        chatClient.on("chatMessageReceived", listenerId, (JSONObject payload) -> {
-            eventHandlerCalled++;
-            eventPayload = payload;
+        logAndToast("Register a test listener");
 
-            Log.i(TAG, eventHandlerCalled + " messages handled.");
-            System.out.printf("Message received! Content is %s.", eventPayload);
-            Log.i(TAG, payload.toString());
-        });
+        chatClient.on("chatMessageReceived", listenerId)
+            .thenApplyAsync((JSONObject payload) -> {
+                eventHandlerCalled++;
+                eventPayload = payload;
+
+                Log.i(TAG, eventHandlerCalled + " messages handled.");
+                System.out.printf("Message received! Content is %s.", eventPayload);
+                Log.i(TAG, payload.toString());
+            });
     }
 
     public void actionUnregisterATestListener(View view) {
         // Act - subscribe
-        log("Unregister a test listener");
+        logAndToast("Unregister a test listener");
         chatClient.off("chatMessageReceived", listenerId);
     }
 }
